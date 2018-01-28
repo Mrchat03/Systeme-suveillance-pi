98bbdd9ca16a713d4b5fca003797499034a12d99
Mrchat03 <34311270+Mrchat03@users.noreply.github.com>
2018-01-28 22:48:08 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,59 @@
+import RPi.GPIO as GPIO
+import time
+import configparser
+import datetime 
+
+config = configparser.ConfigParser()
+config.read_file(open('config_fichier.cfg'))
+
+#GPIO
+GPIO.setmode(GPIO.BOARD)
+capteur=7
+GPIO.setup(capteur, GPIO.IN)
+
+class info_systeme():
+    
+    def __init__(self):
+        self.nom_section_photo = 'PHOTO'
+        self.nom_section_defaut = 'DEFAUT'
+        self.nom_date = 'date'
+        self.nom_photo_total = 'nombre_photo_total'
+        self.nom_photo_jour = 'nombre_photo_jour'
+   
+    def set_date(self):
+        
+   
+    def get_date(self):
+        self.date_config = config.get(self.nom_section_defaut ,self.nom_date)
+        self.date_systeme = str(datetime.date.today())
+    
+    def get_valeur(self):
+        self.val_photo_total = config.getint(self.nom_section_photo,self.nom_photo_total)
+        self.val_photo_jour = config.getint(self.nom_section_photo,self.nom_photo_jour)
+    
+    def ecrire_config(self,val,nom):
+        val += 1;
+        val = str(val)
+        config.set(self.nom_section_photo,nom,val)
+        with open('config_fichier.cfg', 'w') as config_fichier:
+            config.write(config_fichier)       
+
+def photo_mov():
+    print ("Mouvement detecter") 
+    exec(open('photo.py').read())
+    modif.get_date();
+    modif.get_valeur();
+##    if (modif.date_systeme != modif.date_config):
+##        modif.val_photo_jour = 0
+    modif.ecrire_config(modif.val_photo_total,modif.nom_photo_total)
+    modif.ecrire_config(modif.val_photo_jour,modif.nom_photo_jour)
+    time.sleep(2)
+
+modif=info_systeme()
+while True:
+    
+    photo_mov()
+    time.sleep(5)
+    
+    
+    
--- photo.py
+++ photo.py
@@ -8,32 +8,11 @@ config.read_file(open('config_fichier.cfg'))
 class Photo():
     
     nom_section_photo = 'PHOTO'
-    nom_photo_total = 'nombre_photo_total'
-    
-    def __init__(self):
-        self.nom = self.nom_photo_total;
-        self.get_valeur();
-    
-    def get_valeur(self):
-        self.val = config.getint(self.nom_section_photo,self.nom);
-    
-    def ecrire_config(self):      
-        self.val += 1;
-        self.val = str(self.val)
-        config.set(self.nom_section_photo,self.nom,self.val)
-        with open('config_fichier.cfg', 'w') as config_fichier:
-            config.write(config_fichier)
-            
-class Photo_jour(Photo):
-    
     nom_largeur = 'largeur_photo'
     nom_longueur = 'longueur_photo'
     nom_frames = 'frame'
-    nom_photo_jour = 'nombre_photo_jour'
     
     def __init__(self):
-        self.nom = self.nom_photo_jour;
-        self.get_valeur();
         self.get_frames();
         self.get_dimensions();
     
@@ -50,21 +29,17 @@ class Photo_jour(Photo):
             yield 'Images/image%02d.jpg' % frame
             frame += 1
             
-photo_total = Photo()
-photo_jour = Photo_jour()
-
+photo = Photo()
 
 with picamera.PiCamera() as camera:
-    camera.resolution = (photo_jour.longueur,photo_jour.largeur)
+    camera.resolution = (photo.longueur,photo.largeur)
     camera.framerate = 30
     camera.start_preview()
     # Temporisation
     time.sleep(1)
     start = time.time()
-    camera.capture_sequence(photo_jour.nom_image(), use_video_port=True)
+    camera.capture_sequence(photo.nom_image(), use_video_port=True)
     finish = time.time()
-    photo_total.ecrire_config()
-    photo_jour.ecrire_config()
+
 
     
-
